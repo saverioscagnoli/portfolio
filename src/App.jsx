fd7f33dbd6a2a71b80de8b40c04e257b68a3f427
saverioscagnoli <ssaverioscagnoli@gmail.com>
2022-02-22 00:50:27 +0100

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import './style.css'
 import Bio from "./components/Bio";
 import Ilove from "./components/Ilove";
 import Social from "./components/Social";
+import Favicon from "react-favicon";
+import { useState } from "react";
 
 
 
 function App() {
   return (
     <Container>
+       <Favicon url='/favicon.gif' animated={useState(true)} />
       <Navbar />
       <Naps></Naps>
       <Header />
